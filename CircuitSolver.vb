b3d739ccc1116fd7a140e09e32530842a01abcdb
Lane Coleman <lane.coleman@live.com>
2020-09-08 10:10:12 -0600

    Solves for Reactances

--- CircuitSolver.vb
+++ CircuitSolver.vb
@@ -13,7 +13,7 @@
 
     End Sub
     Sub Lab3Oscillator()
-        Dim R1, R2, C1, C2, C3, C4, C5, CP, CGS, CGD, CDS, L1, L2, F As String
+        Dim R1, R2, C1, C2, C3, C4, C5, CP, CGS, CGD, CDS, L1, L2, F As Double
         Dim XC1, XC2, XC3, XC4, XC5, XCP, XCGS, XCGD, XCDS, XL1, XL2 As Double
         Dim goodData As Boolean
         Const PI = 3.14159265359
@@ -21,33 +21,33 @@
         Do
             Try
                 Console.WriteLine("What is the value of R1?")
-                R1 = CInt(Console.ReadLine)
+                R1 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of R2?")
-                R2 = CInt(Console.ReadLine)
+                R2 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of C1?")
-                C1 = CInt(Console.ReadLine)
+                C1 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of C2?")
-                C2 = CInt(Console.ReadLine)
+                C2 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of C3?")
-                C3 = CInt(Console.ReadLine)
+                C3 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of C4?")
-                C4 = CInt(Console.ReadLine)
+                C4 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of C5?")
-                C5 = CInt(Console.ReadLine)
+                C5 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of CProbe?")
-                CP = CInt(Console.ReadLine)
+                CP = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of CGS?")
-                CGS = CInt(Console.ReadLine)
+                CGS = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of CGD?")
-                CGD = CInt(Console.ReadLine)
+                CGD = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of CDS?")
-                CDS = CInt(Console.ReadLine)
+                CDS = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of L1?")
-                L1 = CInt(Console.ReadLine)
+                L1 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of L2?")
-                L2 = CInt(Console.ReadLine)
+                L2 = CDbl(Console.ReadLine)
                 Console.WriteLine("What is the value of F?")
-                F = CInt(Console.ReadLine)
+                F = CDbl(Console.ReadLine)
 
                 goodData = True
             Catch
@@ -55,6 +55,29 @@
                 goodData = False
             End Try
         Loop Until goodData = True
+
+        XC1 = (1 / (2 * PI * F * C1))
+        XC2 = (1 / (2 * PI * F * C2))
+        XC3 = (1 / (2 * PI * F * C3))
+        XC4 = (1 / (2 * PI * F * C4))
+        XC5 = (1 / (2 * PI * F * C5))
+        XCP = (1 / (2 * PI * F * CP))
+        XCGS = (1 / (2 * PI * F * CGS))
+        XCGD = (1 / (2 * PI * F * CGD))
+        XCDS = (1 / (2 * PI * F * CDS))
+        XL1 = (2 * PI * F * L1)
+        XL2 = (2 * PI * F * L2)
+
+        Console.WriteLine("XC1 = " & XC1)
+        Console.WriteLine("XC2 = " & XC2)
+        Console.WriteLine("XC3 = " & XC3)
+        Console.WriteLine("XC4 = " & XC4)
+        Console.WriteLine("XC5 = " & XC5)
+        Console.WriteLine("XCP = " & XCP)
+        Console.WriteLine("XCGS = " & XCGS)
+        Console.WriteLine("XCGD = " & XCGD)
+        Console.WriteLine("XCDS = " & XCDS)
+        Console.ReadLine()
 
 
 
